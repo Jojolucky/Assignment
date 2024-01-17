b009adaf05a1a52020598a73afb7a22b26c9a70b
NCUTemp <jiaohelucky@gmail.com>
2024-01-16 18:58:49 -0800

    test

--- 6150classcoding/6150final/src/Components/GlobalNav.jsx
+++ 6150classcoding/6150final/src/Components/GlobalNav.jsx
@@ -1,38 +1,43 @@
 import { useState } from "react";
-import { nav_menu } from "../Data/structure_data";  
+import { nav_menu } from "../Data/structure_data";
 import "./GlobalNav.css";
 import "../cssgg.css";
 
-function GlobalNav({selectPage, theme}) {
-    const [showMenu, setShowMenu] = useState(false);
-    const globalNavTheme = `global-nav__${theme}`;
-    const li = nav_menu.map(item => {
-        return (
-            <li className="global-nav__item" key={item.name}>
-                <a className={`global-nav__link ${globalNavTheme}`} href={item.path} onClick={e => {
-                    selectPage(e);
-                    setShowMenu(false);
-                }}>
-                    {item.name}
-                </a>
-            </li>
-        )
-    });
-    const showClass = showMenu? "global-nav__list--open" : "";
+function GlobalNav({ selectPage, theme }) {
+  const [showMenu, setShowMenu] = useState(false);
+  const globalNavTheme = `global-nav__${theme}`;
+  const li = nav_menu.map((item) => {
     return (
-        <>
-            <nav className={"global-nav"}>
-                <button className="global-nav__button" onClick={() => {
-                    setShowMenu(!showMenu)
-                }}
-                aria-label={showMenu? "Close Menu" : "Open Menu"}>
-                    <i className="gg-menu"/>
-                </button>
-                <ul className={`global-nav__list ${showClass}`}>
-                    {li}
-                </ul>
-            </nav>
-        </>
-    )
+      <li className="global-nav__item" key={item.name}>
+        <a
+          className={`global-nav__link ${globalNavTheme}`}
+          href={item.path}
+          onClick={(e) => {
+            selectPage(e);
+            setShowMenu(false);
+          }}
+        >
+          {item.name}
+        </a>
+      </li>
+    );
+  });
+  const showClass = showMenu ? "global-nav__list--open" : "";
+  return (
+    <>
+      <nav className={"global-nav"}>
+        <button
+          className="global-nav__button"
+          onClick={() => {
+            setShowMenu(!showMenu);
+          }}
+          aria-label={showMenu ? "Close Menu" : "Open Menu"}
+        >
+          <i className="gg-menu" />
+        </button>
+        <ul className={`global-nav__list ${showClass}`}>{li}</ul>
+      </nav>
+    </>
+  );
 }
 export default GlobalNav;
